1fdbbe83c9fed614eacbac9f3679a7478d308c14
lqhuang <lqhuang@outlook.com>
2016-08-04 15:12:17 +0800

    test

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 import os
+import numpy as np 
+import matplotlib.pyplot as plt
 from cryoio import mrc
 
 V = mrc.readMRC("./particle/1AON.mrc")
+
+plt.plot([1,2,3,4], [2,4,6,8])
+plt.show()
+# mlab.contour3d(V)
 
 print("Hello")
