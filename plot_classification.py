31525134fcd0388d96b4197c6c33134db4520b15
lqhuang <lqhuang@outlook.com>
2016-08-19 18:34:16 +0800

    testing plot particle projections

---
+++ plot_classification.py
@@ -0,0 +1,20 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+from cryoio import mrc
+import geom
+
+
+
+file = 'EMD-2325.map'
+
+model = mrc.readMRC(file)
+
+R1 = geom.rotmat3D_EA(0, 0, 0)
+R2 = geom.rotmat3D_EA(np.pi/2, np.pi/2, np.pi/2)
+
+
+proj1 = np.sum(model, axis=2)
+
+plt.imshow(proj1)
+plt.show
