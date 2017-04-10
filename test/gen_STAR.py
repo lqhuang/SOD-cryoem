913e973b93dc764b2813a441f8a9cc2333bcb6d6
Lanqing Huang <lqhuang@outlook.com>
2017-04-10 17:20:29 +0800

    Add test module

---
+++ test/gen_STAR.py
@@ -0,0 +1,92 @@
+import os
+import time
+
+import numpy as np
+
+from cryoio import mrc
+from cryoio import star
+
+# from util import run_system_command
+"""
+loop_ 
+_rlnImageName #1 
+_rlnAngleRot #2
+_rlnAngleTilt #3
+_rlnAnglePsi #4 
+_rlnOriginX #5
+_rlnOriginY #6 
+"""
+
+# def timetest():
+if __name__ == '__main__':
+    fname = '{0}/test_profiles.star'.format('./particle')
+    # label_names = ['ImageName', 'AngleRot', 'AngleTilt', 'AnglePsi', 'OriginX', 'OriginY']
+    label_names = ['ImageName', 'AngleRot', 'AngleTilt', 'AnglePsi']
+    # label_names = ['AngleRot', 'AngleTilt', 'AnglePsi']
+
+    step = 30.0
+    rot = np.arange(0, 360.0, step)
+    tilt = np.arange(0, 180.0+step, step)
+    psi = np.arange(0, 360.0, step)
+
+    imgs_length = rot.shape[0] * tilt.shape[0] * psi.shape[0]
+
+    xshift = np.zeros(imgs_length)
+    yshift = np.zeros(imgs_length)
+
+    metadata = dict()
+    for label in label_names:
+        metadata[label] = list()
+    for idx in range(imgs_length):
+        i, j, k = np.unravel_index(idx, [rot.shape[0], tilt.shape[0], psi.shape[0]])
+        metadata['ImageName'].append(idx)
+        metadata['AngleRot'].append(rot[i])
+        metadata['AngleTilt'].append(tilt[j])
+        metadata['AnglePsi'].append(psi[k])
+        # metadata['OriginX'].append(xshift[idx])
+        # metadata['OriginY'].append(yshift[idx])
+
+    a = metadata['ImageName']
+    print(a[0])
+    print(metadata['ImageName'][0])
+    b = metadata['AnglePsi']
+    star.writeSTAR('./particle/test_profiles.star', \
+                   imgs_path='Users/lqhuang/Git/Test/particle/projections.mrcs', AnglePsi=b, ImageName=a)
+    print('a', a[0])
+    print(metadata['ImageName'][0])
+    # print('Calculate projections')
+    # tic = time.time()
+    # project_cmd = 'relion_project --i {0}/1AON.mrc --o {0}/projections --ang {0}/test_profiles.star --angpix 3.0'.format('/Users/lqhuang/Git/Test/particle')
+    # info = run_system_command(project_cmd)
+    # print(info)
+    # print('projection times: ', time.time()-tic)
+
+    # data = mrc.readMRCimgs('./particle/projections.mrcs', 0)
+
+    # x, y, leng = data.shape
+    # cx = int(x/2)
+    # cy = int(y/2)
+
+    # resave = np.zeros_like(data)
+
+    # xarr = np.arange(0, x) - float(cx)
+    # yarr = np.arange(0, y) - float(cy)
+    # xx, yy = np.meshgrid(xarr,yarr)
+    # mask = ((xx**2 + yy*2) < 0) * 1.0
+
+    # for idx in range(leng):
+
+    #     mean_img = np.mean(data[:,:,idx].flatten())
+    #     var_img = np.var(data[:,:,idx].flatten())
+    #     resave[:,:,idx] = (data[:,:,idx] - mean_img) / np.sqrt(var_img)
+    #     resave[:,:,idx] *= mask
+
+    # mrc.writeMRC('./particle/projections.mrcs', resave)
+
+
+    # print('Calculate reconstruction')
+    # tic = time.time()
+    # reconstruct_cmd = 'relion_reconstruct --i {0}/projections.star --o {0}/test.mrc --angpix 3.0 --j 8 --fsc True'.format('/Users/lqhuang/Git/Test/particle')
+    # info = run_system_command(reconstruct_cmd)
+    # # print(info)
+    # print('reconstruction times: ', time.time()-tic)
---
+++ test/gen_projections.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+from cryoio import mrc
+import cryoem, density, cryoops, geom
+
+import pyximport; pyximport.install(setup_args={"include_dirs":np.get_include()},reload_support=True)
+import sincint
+
+from time import time
+
+phantompath = './particle/1AON.mrc'
+
+N = 128
+rad = 0.95
+shift_sigma = 3.0
+sigma_noise = 25.0
+
+M_totalmass = 80000
+
+kernel = 'lanczos'
+ksize = 6
+
+M = mrc.readMRC(phantompath)
+
+tic = time()
+premult = cryoops.compute_premultiplier(N, kernel, ksize)
+
+TtoF = sincint.gentrunctofull(N=N, rad=rad)
+
+cryoem.window(M, 'circle') # apply a sphere mask
+M[M<0] = 0
+if M_totalmass is not None:
+    M *= M_totalmass / M.sum()
+
+V = density.real_to_fspace( premult.reshape((1,1,-1)) * premult.reshape((1,-1,1)) * premult.reshape((-1,1,1)) * M )
+
+# print('preprocessing time: ', time()-tic)
+pardata = {'R':[],'t':[]}
+
+tic = time()
+# Randomly generate the viewing direction/shift
+pt = np.random.randn(3)
+pt /= np.linalg.norm(pt)
+psi = 2*np.pi*np.random.rand()
+EA = geom.genEA(pt)[0]
+# print(EA)
+EA[2] = psi
+shift = np.random.randn(2) * shift_sigma
+
+R = geom.rotmat3D_EA(*EA)[:,0:2]
+print(R)
+slop = cryoops.compute_projection_matrix([R], N, kernel, ksize, rad, 'rots')
+shift_phases = cryoops.compute_shift_phases(shift.reshape((1,2)), N, rad)[0]
+
+D = slop.dot( V.reshape((-1,)) )
+D *= shift_phases
+
+# imgdata = density.fspace_to_real( (TtoF.dot(D)).reshape((N,N)) ) + np.require(np.random.randn(N, N)*sigma_noise, dtype=density.real_t)
+imgdata = density.fspace_to_real( (TtoF.dot(D)).reshape((N,N)) )
+
+# genctf_stack.add_img(genctfI,
+#                         PHI=EA[0]*180.0/np.pi,THETA=EA[1]*180.0/np.pi,PSI=EA[2]*180.0/np.pi,
+#                         SHX=shift[0],SHY=shift[1])
+print('generate a projection: ', time()-tic)
+pardata['R'].append(R)
+pardata['t'].append(shift)
