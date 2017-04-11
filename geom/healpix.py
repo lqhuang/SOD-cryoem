0dae7836892b8cb85540d78d51f95e51cf6ad089
Lanqing Huang <lqhuang@outlook.com>
2017-04-11 19:54:20 +0800

    Recoding HealPix module

--- geom/healpix.py
+++ geom/healpix.py
@@ -1,7 +1,6 @@
 import numpy as np
 import healpy as hp
 
-import geom
 
 # given nside | number of pixels | resolution (pixel size in degree) | Maximum angular distance (degree) | pixel area (in square degrees)
 #      1      |        12        |              58.6323              |              48.1897              |          3437.746771
@@ -15,36 +14,4 @@ import geom
 #     256     |      786432      |              0.2290               |              0.2391               |            0.052456
 #     512     |     3145728      |              0.1145               |              0.1196               |            0.013114
 #    1024     |     12582912     |              0.0573               |              0.0598               |            0.003278
-
-def calculate_nside_resolution():
-    NSIDE = [2**i for i in range(11)]
-    print('given nside | number of pixels | resolution (pixel size in degree) | Maximum angular distance (degree) | pixel area (in square degrees)')
-    for nside in NSIDE:
-        npix = hp.nside2npix(nside)
-        resol = np.rad2deg(hp.nside2resol(nside))
-        maxrad = np.rad2deg(hp.max_pixrad(nside))
-        pixarea = hp.nside2pixarea(nside, degrees=True)
-        print('{0:^11} | {1:^16} | {2:^33.4f} | {3:^33.4f} | {4:^30.6f}'.format(nside, npix, resol, maxrad, pixarea))
-
-if __name__ == '__main__':
-    # calculate_nside_resolution()
-    # nside = 32
-    # hp.mollview(m, nest=True)
-
-    v = np.random.randn(1000,3)
-    # v = v / np.tile(np.linalg.norm(v, axis=1), [3, 1]).T
-    v = v  / np.linalg.norm(v, axis=1).repeat(3).reshape(-1,3)
-
-    EA = geom.genEA(v)
-
-    phi = EA[:, 0]
-    theta = EA[:, 1]
-
-    hp.mollview()
-
-    hp.visufunc.projscatter(theta, phi, 'r.')
-
-    phi += 2 * np.pi
-
-    # hp.visufunc.proj(theta, phi, 'b.')
 
--- test/healpy_test.py
+++ test/healpy_test.py
@@ -1,6 +1,8 @@
 import numpy as np
 import healpy as hp
+from matplotlib import pyplot as plt
 
+from geom import geom
 
 # given nside | number of pixels | resolution (pixel size in degree) | Maximum angular distance (degree) | pixel area (in square degrees)
 #      1      |        12        |              58.6323              |              48.1897              |          3437.746771
@@ -27,4 +29,18 @@ def calculate_nside_resolution():
 
 
 if __name__ == '__main__':
+
     calculate_nside_resolution()
+
+    # generate random distribution of Euler angles
+    v = np.random.randn(100,3)
+    v = v  / np.linalg.norm(v, axis=1).repeat(3).reshape(-1,3)
+    EA = geom.genEA(v)
+    phi = EA[:, 0]
+    # phi += 2 * np.pi
+    theta = EA[:, 1]
+    # visulization
+    hp.mollview()
+    hp.visufunc.projscatter(theta, phi, 'r.')
+    hp.graticule()
+    plt.show()
