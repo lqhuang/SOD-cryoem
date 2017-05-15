3db65d2b3345ba9593bfeeb966f90bd22575e9b0
lqhuang <lqhuang@outlook.com>
2017-05-15 12:36:28 +0800

    Update and debug

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-import os.path
-import tempfile
+import os
+import time
+import argparse
 import multiprocessing as mp
 
 import numpy as np
 import healpy as hp
 
 from cryoio import star, mrc
-from cryoem import relion, cryoem
+from cryoem import relion, xmipp
+from geom import geom
+import density
 
 
 """
@@ -17,9 +20,12 @@ undone:
 2. standard image preprocessing
 """
 
-WD = '/Users/lqhuang/Git/SOD-cryoem/data/job'
-# temp_directory = tempfile.mkdtemp()
+WD = '/home/lqhuang/Git/SOD-cryoem/data/job2'
+if not os.path.exists(WD):
+    os.mkdir(WD)
 temp_directory = os.path.join(WD, 'tmp')
+if not os.path.exists(temp_directory):
+    os.mkdir(temp_directory)
 
 arr_type = type(np.ctypeslib.as_ctypes(np.float32()))
 
@@ -35,8 +41,11 @@ def gen_exp_samples(num_EAs, phantompath, data_dst):
         EA[2] = psi
         EAs.append(np.rad2deg(EA))
 
+    EAs_grid = gen_ref_EAs_grid()
+    # grid_size = EAs_grid.shape[0]
+    # EAs = EAs_grid[np.random.randint(0, grid_size, size=num_EAs)]
     exp_star = data_dst + '_gen.star'
-    star.easy_writeSTAR(exp_star, EAs=EAs)
+    star.easy_writeSTAR_relion(exp_star, EAs=EAs)
     projs_star, projs_mrcs = relion.project(phantompath, data_dst,
                                             ang=exp_star)
     return projs_star, projs_mrcs
@@ -45,6 +54,7 @@ def gen_exp_samples(num_EAs, phantompath, data_dst):
 def gen_ref_EAs_grid(nside=8, psi_step=10):
 
     npix = hp.nside2npix(nside)
+    resol = np.rad2deg(hp.nside2resol(nside))
     theta, phi = hp.pix2ang(nside, range(npix))
     theta, phi = np.rad2deg(theta), np.rad2deg(phi)
     psi = np.arange(0, 360, psi_step)
@@ -53,40 +63,49 @@ def gen_ref_EAs_grid(nside=8, psi_step=10):
     grid_phi, _ = np.meshgrid(phi, psi)
     EAs_tuple = (grid_phi.flatten(), grid_theta.flatten(), grid_psi.flatten())
     EAs_grid = np.vstack(EAs_tuple).T
-
+    print('number of points on shpere: {0}. \n'
+          'resolution: {1:.2f} degree, step of inplane-rotation: {2} degree\n'
+          'total grid size: {3}'.format(npix, resol, psi_step, npix*360/psi_step))
     return EAs_grid
 
 
 def gen_mrcs_from_EAs(EAs, phantompath, dstpath):
 
     ang_star = os.path.join(temp_directory, 'EAs.star')
-    star.easy_writeSTAR(ang_star, EAs=EAs)
+    star.easy_writeSTAR_relion(ang_star, EAs=EAs)
     projs_star, projs_mrcs = relion.project(phantompath, dstpath, ang=ang_star)
-    model_proj_imgs = mrc.readMRCimgs(projs_mrcs, idx=0)
+    model_projs = mrc.readMRCimgs(projs_mrcs, idx=0)
 
-    return model_proj_imgs
+    return model_projs
 
 
-def find_similar(input_proj, ref_projs):
-    grid_size = 0
+def standard_processing(img_or_imgstack):
+    pass
+
+
+def find_similar(exp_proj):
+    tic = time.time()
+    grid_size = model_proj_imgs.shape[2]
     likelihood = np.zeros(grid_size)
     for i in range(grid_size):
-        ref_proj = np.ctypeslib.as_array(ref_proj_stack[:, :, i])
-        p = np.sum((input_proj - ref_proj) ** 2 / (-2 * input_proj))
+        ref_proj = model_proj_imgs[:, :, i]
+        p = np.mean((exp_proj - ref_proj) ** 2 / (-2 * exp_proj ** 2))
         likelihood[i] = p
-
-    idx = np.argmin(likelihood)
+    idx = np.argmax(likelihood)
+    toc = time.time() - tic
+    if toc > 3:
+        print('\r{0} forloops cost {1:.4f} seconds.'.format(grid_size, toc), end='')
     return idx
 
 
-def projection_matching(input_model, projs, dir_suffix=None):
+def projection_matching(input_model, projs, nside=8, dir_suffix=None):
     """
     Parameters:
     -------
     input_model: path of input mrc file
     projections: numpy array
     """
-    EAs_grid = gen_ref_EAs_grid(nside=8)
+    EAs_grid = gen_ref_EAs_grid(nside=nside)
     if dir_suffix:
         dstpath = os.path.join(WD, dir_suffix, 'model_projections')
     else:
@@ -95,52 +114,74 @@ def projection_matching(input_model, projs, dir_suffix=None):
     N = projs.shape[0]
     num_projs = projs.shape[2]
     num_model_imgs = EAs_grid.shape[0]
-    num_threads = 4
+    num_threads = mp.cpu_count()
+    tic = time.time()
+    global model_proj_imgs
     model_proj_imgs = gen_mrcs_from_EAs(EAs_grid, input_model, dstpath)
+    print('Time to recover projections from mrcs file: {0:.4f} s'.format(
+        time.time() - tic))
 
-    def find_similar(input_proj):
-        grid_size = model_proj_imgs.shape[2]
-        likelihood = np.zeros(grid_size)
-        for i in range(grid_size):
-            ref_proj = model_proj_imgs[:, :, i]
-            p = np.sum((exp_proj - ref_proj) ** 2 / (-2 * exp_proj))
-            likelihood[i] = p
-        idx = np.argmin(likelihood)
-        return idx
-
-    pool = mp.Pool(processes=num_threads)
-    idx_orientations = np.zeros(num_projs)
-    idx_orientations = pool.map(
-        find_similar, (projs[:, :, i] for i in range(num_projs)))
+    print('Projection matching: multiprocess start')
+    with mp.Pool(processes=num_threads) as pool:
+        idx_orientations = pool.map(
+            find_similar, (projs[:, :, i] for i in range(num_projs)))
+    print('\nFinish orientation!')
     orientations = EAs_grid[idx_orientations]
     return orientations
 
 
-def reconstruct(projs_path):
+def reconstruct(projs_path, nside=8):
 
     exp_samples = mrc.readMRCimgs(projs_path, idx=0)
-    N, _, num_samples = exp_samples.shape
-
-    M = cryoem.generate_phantom_density(N, 0.95 * N / 2.0, 5 * N / 128.0, 30)
+    input_shape = exp_samples.shape
+    print('size of input images: {0}x{1}, number of input images: {2}'.format(*input_shape))
+    print('generating random phantom density')
+    N = input_shape[0]
+    M = density.generate_phantom_density(N, 0.95 * N / 2.0, 5 * N / 128.0, 30)
     output_mrc = os.path.join(WD, 'initial_random_model.mrc')
     mrc.writeMRC(output_mrc, M)
-
+    print('Start projection matching')
     total_iteration = 20
     for it in range(total_iteration):
+        print('Iteration {0}'.format(it))
+        dir_suffix = 'it' + str(it).zfill(3)
+        iter_dir = os.path.join(WD, dir_suffix)
+        if not os.path.exists(iter_dir):
+            os.mkdir(iter_dir)
+
+        tic = time.time()
 
         input_model = output_mrc
-        ori = projection_matching(input_model, exp_samples)
+        ori = projection_matching(input_model, exp_samples, nside=nside, dir_suffix=dir_suffix)
 
-        dir_suffix = 'it' + str(it).zfill(3)
         ori_star = os.path.join(WD, dir_suffix, 'orientations.star')
-        output_mrc = os.path.join(WD, dir_suffix, dir_suffix + '_reco.mrc')
+        star.easy_writeSTAR_xmipp(ori_star, EAs=ori, imgs_path=projs_path)
 
-        star.easy_writeSTAR(ori_star, EAs=ori, imgs_path=projs_path)
-        relion.reconstruct(ori_star, output_mrc)
+        output_mrc = os.path.join(WD, dir_suffix, dir_suffix + '_reco.mrc')
+        xmipp.reconstruct_fourier(ori_star, output_mrc, thr=4)
+
+        toc = time.time() - tic
+
+        print('Iteration {0} finished, spread time: {1:.4f} seconds'.format(it, toc))
+        print('Total iteration is {0}, remian {1:.2f} minutes'.format(
+            total_iteration, (total_iteration-it)*toc/60))
+        print('---------------------------------------------------------------------------------')
 
 
 if __name__ == '__main__':
-    exp_star, exp_mrcs = gen_exp_samples(10000,
-                                         '/Users/lqhuang/Git/SOD-cryoem/particle/EMD-6044.mrc',
-                                         '/Users/lqhuang/Git/SOD-cryoem/data/job/exp_projections')
-    reconstruct(exp_mrcs)
+    paser = argparse.ArgumentParser()
+    paser.add_argument('-n', '--num_projs', help='generate N projections for simulation', type=int)
+    paser.add_argument('--nside', help='size number for healpix to generate reference grid.', type=int)
+    paser.add_argument('--print_to_file', type=bool, default=False)
+    args = paser.parse_args()
+    num = args.num_projs
+    print_to_file = args.print_to_file
+    if print_to_file:
+        import sys
+        sys.stdout = open(os.path.join(WD, 'log.txt'), 'w')
+    exp_star, exp_mrcs = gen_exp_samples(num,
+                                         'particle/EMD-6044.mrc',
+                                         os.path.join(WD, 'exp_projections'))
+    reconstruct(exp_mrcs, nside=32)
+    if print_to_file:
+        sys.stdout.close()
