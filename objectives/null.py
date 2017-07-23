e544160ebed1597e9137a3dff16da8db02d748a1
Lanqing Huang <lqhuang@outlook.com>
2017-07-23 18:43:52 +0800

    Add open source codes

---
+++ objectives/null.py
@@ -0,0 +1,33 @@
+from .objective import Objective
+
+import numpy as np
+
+class NullPrior(Objective):
+    def __init__(self,fspace):
+        Objective.__init__(self,fspace)
+
+    def set_params(self,cparams):
+        self.params = cparams
+
+    def get_preconditioner(self,precond_type):
+        return 0
+    
+    def scalar_eval(self,vals,compute_gradient=False):
+        if compute_gradient:
+            return np.ones_like(vals),np.zeros_like(vals)
+        else:
+            return np.ones_like(vals) 
+
+    def eval(self, M, compute_gradient=True, fM=None, **kwargs):
+        outputs = {}
+
+        if compute_gradient:
+            if self.fspace:
+                dM = density.zeros_like(fM)
+            else:
+                dM = density.zeros_like(M)
+            return 0.0, dM, outputs
+        else:
+            return 0.0, outputs
+
+
