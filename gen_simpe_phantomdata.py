b5b97da162b243033860038130fbb8aa0bfbbe54
Lanqing Huang <lqhuang@outlook.com>
2017-07-23 15:59:28 +0800

    Add data generating scripts

--- gen_simpe_phantomdata.py
+++ gen_simpe_phantomdata.py
@@ -10,7 +10,6 @@ sys.path.append(os.path.join(os.path.dirname(os.path.abspath(
 
 import time
 from cryoio import mrc
-from cryoio.ctfstack import CTFStack, GeneratedCTFStack
 from cryoem import cryoem
 import density
 import cryoops
@@ -28,11 +27,7 @@ import pyximport; pyximport.install(
 import sincint
 
 
-def genphantomdata(N_D, phantompath, ctfparfile):
-    # mscope_params = {'akv': 200, 'wgh': 0.07,
-    #                  'cs': 2.0, 'psize': 2.8, 'bfactor': 500.0}
-    mscope_params = {'akv': 200, 'wgh': 0.07,
-                     'cs': 2.0, 'psize': 3.0, 'bfactor': 500.0}
+def genphantomdata(N_D, phantompath):
     
     M = mrc.readMRC(phantompath)
 
@@ -51,19 +46,12 @@ def genphantomdata(N_D, phantompath, ctfparfile):
     N_D = int(N_D)
     N = int(N)
     rad = float(rad)
-    psize = mscope_params['psize']
-    bfactor = mscope_params['bfactor']
     shift_sigma = float(shift_sigma)
     sigma_noise = float(sigma_noise)
     M_totalmass = float(M_totalmass)
 
-    srcctf_stack = CTFStack(ctfparfile, mscope_params)
-    genctf_stack = GeneratedCTFStack(mscope_params, parfields=[
-                                     'PHI', 'THETA', 'PSI', 'SHX', 'SHY'])
 
     TtoF = sincint.gentrunctofull(N=N, rad=rad)
-    Cmap = np.sort(np.random.random_integers(
-        0, srcctf_stack.get_num_ctfs() - 1, N_D))
 
     cryoem.window(M, 'circle')
     M[M < 0] = 0
@@ -79,20 +67,12 @@ def genphantomdata(N_D, phantompath, ctfparfile):
 
     pardata = {'R': [], 't': []}
 
-    prevctfI = None
-    for i, srcctfI in enumerate(Cmap):
+    for i in range(N_D):
         ellapse_time = time.time() - tic
         remain_time = float(N_D - i) * ellapse_time / max(i, 1)
         print("\r%.2f Percent.. (Elapsed: %s, Remaining: %s)" % (i / float(N_D)
                                                                  * 100.0, format_timedelta(ellapse_time), format_timedelta(remain_time)))
         sys.stdout.flush()
-
-        # Get the CTF for this image
-        cCTF = srcctf_stack.get_ctf(srcctfI)
-        if prevctfI != srcctfI:
-            genctfI = genctf_stack.add_ctf(cCTF)
-            C = cCTF.dense_ctf(N, psize, bfactor).reshape((N**2,))
-            prevctfI = srcctfI
 
         # Randomly generate the viewing direction/shift
         pt = np.random.randn(3)
@@ -100,59 +80,55 @@ def genphantomdata(N_D, phantompath, ctfparfile):
         psi = 2 * np.pi * np.random.rand()
         EA = geom.genEA(pt)[0]
         EA[2] = psi
-        shift = np.random.randn(2) * shift_sigma
+        # shift = np.random.randn(2) * shift_sigma
+        shift = np.asarray([0, 0])
 
         R = geom.rotmat3D_EA(*EA)[:, 0:2]
         slop = cryoops.compute_projection_matrix(
             [R], N, kernel, ksize, rad, 'rots')
         S = cryoops.compute_shift_phases(shift.reshape((1, 2)), N, rad)[0]
+        # S = np.asarray([0, 0])
 
         D = slop.dot(V.reshape((-1,)))
         D *= S
 
-        imgdata[i] = density.fspace_to_real((C * TtoF.dot(D)).reshape((N, N))) + np.require(
+        imgdata[i] = density.fspace_to_real(TtoF.dot(D).reshape((N, N))) + np.require(
             np.random.randn(N, N) * sigma_noise, dtype=density.real_t)
-
-        genctf_stack.add_img(genctfI,
-                             PHI=EA[0] * 180.0 / np.pi, THETA=EA[1] * 180.0 / np.pi, PSI=EA[2] * 180.0 / np.pi,
-                             SHX=shift[0], SHY=shift[1])
 
         pardata['R'].append(R)
         pardata['t'].append(shift)
 
     print("\rDone in ", time.time() - tic, " seconds.")
-    return imgdata, genctf_stack, pardata, mscope_params
+    return imgdata, pardata
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 5:
+    if len(sys.argv) != 4:
         print("""Wrong Number of Arguments. Usage:
 %run genphantomdata num inputmrc inputpar output
 %run genphantomdata 5000 finalphantom.mrc Data/thermus/thermus_Nature2012_128x128.par Data/phantom_5000
 """)
         sys.exit()
 
-    imgdata, ctfstack, pardata, mscope_params = genphantomdata(sys.argv[1], sys.argv[2], sys.argv[3])
-    outpath = sys.argv[4]
+    print(sys.argv)
+    imgdata, pardata = genphantomdata(sys.argv[1], sys.argv[2])
+    outpath = sys.argv[3]
     if not os.path.isdir(outpath):
         os.makedirs(outpath)
 
     tic = time.time()
-    print("Dumping data..."); sys.stdout.flush()
-    def_path = os.path.join(outpath,'defocus.txt')
-    ctfstack.write_defocus_txt(def_path)
+    print("Dumping data...")
+    sys.stdout.flush()
 
-    par_path = os.path.join(outpath,'ctf_gt.par')
-    ctfstack.write_pardata(par_path)
-
-    mrc_path = os.path.join(outpath,'imgdata.mrc')
+    mrc_path = os.path.join(outpath, 'imgdata.mrc')
     print(os.path.realpath(mrc_path))
-    mrc.writeMRC(mrc_path, np.transpose(imgdata,(1,2,0)), mscope_params['psize'])
+    mrc.writeMRC(mrc_path, np.transpose(
+        imgdata, (1, 2, 0)), psz=3.0)
 
-    pard_path = os.path.join(outpath,'pardata.pkl')
-    print(os.path.realpath(mrc_path))
-    with open(pard_path,'wb') as fi:
+    pard_path = os.path.join(outpath, 'pardata.pkl')
+    print(os.path.realpath(pard_path))
+    with open(pard_path, 'wb') as fi:
         pickle.dump(pardata, fi, protocol=2)
-    print("Done in ", time.time()-tic, " seconds.")
+    print("Done in ", time.time() - tic, " seconds.")
 
     sys.exit()
