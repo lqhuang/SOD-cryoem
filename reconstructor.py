c507bfd4929732c5c59be22019de9458cd4e1633
Lanqing Huang <lqhuang@outlook.com>
2017-07-26 20:57:14 +0800

    Fig bug for module name and change location of logs

--- reconstructor.py
+++ reconstructor.py
@@ -324,7 +324,7 @@ class CryoOptimizer(BackgroundWorker):
         self.io_thread.start()
 
         # General setup ----------------------------------------------------
-        self.expbase = expbase
+        self.expbase = os.path.join(expbase, 'logs')
         self.outbase = None
 
         # Paramter setup ---------------------------------------------------
